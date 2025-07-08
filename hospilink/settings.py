4654eed9cf5589259cfab93b6d71afb62a8f6aa8
Saminouicer <saminouicer3@gmail.com>
2025-07-08 17:36:02 +0100

    fix ALLOWED_HOSTS

--- hospilink/settings.py
+++ hospilink/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = 'django-insecure-2ar%6-c0xcwh-_5l)o0%q2_s#d!gz&h#+gmuw*1f3=wma_7_x*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 
